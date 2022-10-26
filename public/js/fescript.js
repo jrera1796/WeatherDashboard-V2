18569799d9d8d18d3d077006451b788e750a688e
jrera1796 <86329054+jrera1796@users.noreply.github.com>
2022-10-26 16:34:08 -0700

    JSON object from back-end to front-end changes

--- public/js/fescript.js
+++ public/js/fescript.js
@@ -1,34 +1,34 @@
 const cityInput = document.getElementById('searchedcity')
 const cityTitle = document.getElementById('citytitle')
 const cityNow = document.getElementById('citynow')
-
+const cityCast = document.getElementById('citycast')
 function grabWeather(c) {
   if (c == '') {
     console.log('Please enter a city')
   }
   else {
     cityInput.value = ""
-    fetchData(c).then(cityData => {
-      let currentw = cityData[0].current
-      //Fix error handling
-      // if (currentw.cod !== 200) { console.log('Error') }
-      cityTitle.innerHTML = currentw.name
-      let arr = cityData[0].current.main
-
-      Array.from(cityNow.children).forEach(element => {
-        // console.log(element.className)
-        Object.keys(arr).forEach(key => {
-          // console.log(key)
-          if (key === element.className) {
-            element.innerHTML = arr[key]
-            // console.log('match')
-          }
-          else {
-            // console.log('these dont match')
-          }
+    fetchData(c)
+      .then(cityData => {
+        let currentw = cityData[0].current
+        let currentf = cityData[1].forecast
+        let currentMain = currentw.main
+        let forecastArray = currentf.list
+        cityTitle.innerHTML = currentw.name
+        console.log(currentf)
+        Array.from(cityNow.children).forEach(element => {
+          Object.keys(currentMain).forEach(key => {
+            key === element.className ? element.innerHTML = currentMain[key] : console.log('these dont match')
+          })
         })
+
+        Array.from(cityCast.children).forEach(element => {
+          Object.keys(forecastMain).forEach(key => {
+            key === element.className ? element.innerHTML = forecastMain[key] : console.log('these forecasts dont match')
+          })
+        })
+
       })
-    })
   }
 }
 
